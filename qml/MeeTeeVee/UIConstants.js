3a690b7436bb535a37f4bf1b04f42cece03ddca9
J-P Nurmi <jpnurmi@gmail.com>
2012-02-23 22:00:35 +0100

    Updated UIConstants

--- qml/MeeTeeVee/UIConstants.js
+++ qml/MeeTeeVee/UIConstants.js
@@ -1,15 +1,22 @@
 .pragma library
 
 var PAGE_MARGIN = 16;
-var DEFAULT_SPACING = 16;
+var LARGE_SPACING = 16;
+var MEDIUM_SPACING = 6;
+var SMALL_SPACING = 2;
 
+var FONT_FAMILY = "Nokia Pure Text";
+var FONT_FAMILY_LIGHT = "Nokia Pure Text Light";
+
+var HUGE_FONT = 64;
 var LARGE_FONT = 32;
 var MEDIUM_FONT = 26;
 var SMALL_FONT = 18;
 
+var INFO_COLOR = "#282828";
 var TITLE_COLOR = "#ffffff";
 var SUBTITLE_COLOR = "#c8c8c8";
-var ACTITLE_COLOR = "#797979";
+var PRESSED_COLOR = "#797979";
 var SEPARATOR_COLOR = "#b2b2b4";
 
 var DISABLED_OPACITY = 0.4;
