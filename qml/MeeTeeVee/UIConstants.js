13ff4a37a988881381a3e3340cc2cd7285ba8383
J-P Nurmi <jpnurmi@gmail.com>
2012-03-08 23:07:12 +0100

    UIConstants: blue pressed color

--- qml/MeeTeeVee/UIConstants.js
+++ qml/MeeTeeVee/UIConstants.js
@@ -16,7 +16,7 @@ var SMALL_FONT = 18;
 var INFO_COLOR = "#282828";
 var TITLE_COLOR = "#ffffff";
 var SUBTITLE_COLOR = "#c8c8c8";
-var PRESSED_COLOR = "#797979";
+var PRESSED_COLOR = "#4591ff";
 var SEPARATOR_COLOR = "#b2b2b4";
 
 var DISABLED_OPACITY = 0.4;
