e857546ed89feaeda4796cfbc4ce704c6bdf3a39
J-P Nurmi <jpnurmi@gmail.com>
2012-04-11 00:32:04 +0200

    QNetworkRequest::PreferCache

--- main.cpp
+++ main.cpp
@@ -1,18 +1,34 @@
 #include <QtGui/QApplication>
 #include <QtGui/QPixmapCache>
 #include <QtGui/QDesktopServices>
+#include <QtNetwork/QNetworkReply>
+#include <QtNetwork/QNetworkRequest>
 #include <QtNetwork/QNetworkDiskCache>
 #include <QtNetwork/QNetworkAccessManager>
 #include <QtDeclarative/QDeclarativeEngine>
 #include <QtDeclarative/QDeclarativeNetworkAccessManagerFactory>
 #include "qmlapplicationviewer.h"
 
+class NetworkAccessManager : public QNetworkAccessManager
+{
+public:
+    NetworkAccessManager(QObject *parent) : QNetworkAccessManager(parent) { }
+
+protected:
+    virtual QNetworkReply *createRequest(Operation operation, const QNetworkRequest &request, QIODevice *outgoingData = 0)
+    {
+        QNetworkRequest cacheRequest(request);
+        cacheRequest.setAttribute(QNetworkRequest::CacheLoadControlAttribute, QNetworkRequest::PreferCache);
+        return QNetworkAccessManager::createRequest(operation, cacheRequest, outgoingData);
+    }
+};
+
 class NetworkAccessManagerFactory : public QDeclarativeNetworkAccessManagerFactory
 {
 public:
     virtual QNetworkAccessManager *create(QObject *parent)
     {
-        QNetworkAccessManager *nam = new QNetworkAccessManager(parent);
+        QNetworkAccessManager *nam = new NetworkAccessManager(parent);
         QNetworkDiskCache *cache = new QNetworkDiskCache(nam);
         cache->setCacheDirectory(QDesktopServices::storageLocation(QDesktopServices::CacheLocation));
         nam->setCache(cache);
