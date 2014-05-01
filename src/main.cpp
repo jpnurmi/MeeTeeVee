f918bc2b80ccc0d1c0a55bb401cfac12f3acc14b
J-P Nurmi <jpnurmi@gmail.com>
2014-05-01 17:20:14 +0200

    Revert "Fix app name in the about dialog"
    This reverts commit 44c59edaccec60ed741b113f0aa6d78edda3bfe9.
    
    Conflicts:
    	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -42,11 +42,10 @@ static bool removeDir(const QDir &dir)
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
-    QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
-    QGuiApplication::setOrganizationName("harbour-meeteevee");
     QGuiApplication::setApplicationName("MeeTeeVee");
     QGuiApplication::setApplicationVersion("0.2");
 
+    QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     QScopedPointer<QQuickView> viewer(SailfishApp::createView());
 
     QPixmapCache::setCacheLimit(20 * 1024);
